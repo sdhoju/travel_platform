1426e841b7fca993fcdc58104dfe1c390f85d5ed
Sameer Dhoju <sdhoju@go.olmeiss.edu>
2017-04-05 16:19:48 -0500

    add transportation

--- Rails2/travel/app/controllers/transportations_controller.rb
+++ Rails2/travel/app/controllers/transportations_controller.rb
@@ -1,16 +1,26 @@
 class TransportationsController < ApplicationController
-
+ def index
+ 	 @trip = Trip.find(params[:trip_id])
+    @transportations = @trip.transportations.all
+  end
+  def new
+   @trip = Trip.find(params[:trip_id])
+    @transportations = @trip.transportations.new
+  end
+  def show
+  	 @trip = Trip.find(params[:trip_id])
+  	  @transportation = @trip.transportations.find(params[:id])
+  	end
  def create
     @trip = Trip.find(params[:trip_id])
 
     @transportation =@trip.transportations.create(transportation_params)
-    redirect_to trips_path(@trip)
+    redirect_to trip_path(@trip)
   end
    def destroy
    	@trip = Trip.find(params[:trip_id])
     @transportation = @trip.transportations.find(params[:id])
     @transportation.destroy
- 
     redirect_to trip_path(@trip)
   end
  private

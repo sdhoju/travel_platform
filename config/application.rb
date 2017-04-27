cb62c7ff0f145bab81b5d451081c72c7679328b4
Sameer Dhoju <Sdhoju@go.olemiss.edu>
2017-04-26 19:18:35 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,5 @@ module Travel
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  config.assets.initialize_on_precomplie = false
+
 end

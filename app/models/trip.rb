e4202b89859403a72e741895f1ad732af6a8c8d6
Sameer Dhoju <sdhoju@go.olmeiss.edu>
2017-05-03 17:39:37 -0500

    formated the forms

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,15 @@
 class Trip < ApplicationRecord
+validates :tripno, :place, :begindate, :begintime, :enddate, :endtime,
+			:accomp, :dept, :contact, :phone,  presence: true
+
+
+
+
 belongs_to :user
 has_many :transactions, dependent: :destroy 
 has_many :other_expenses, dependent: :destroy 
 has_many :registration_fees, dependent: :destroy 
 has_many :transportations, dependent: :destroy 
 end
+
+
